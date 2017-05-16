ca582b631411a01355d5ce1603f02ee6349a1924
codilar <prakhar@codilar.com>
2017-05-16 14:57:22 +0530

    Colors and comments added

--- development.sh
+++ development.sh
@@ -1,8 +1,40 @@
+#/**
+#	Author : Prakhar Bhardwaj
+#	Email : prakhar20@live.com
+#
+#	Server Setup Wizard
+#
+#	Guidelines 
+#		1) Login As Root To Run This Script
+#		2) Select Server Type
+#		3) Select If You Need Browser
+#		4) To Install IDEs
+#		
+#		
+#		Modules Added Latest Versions 
+#		Apache 2.0
+#		Nginx
+#		Maria-Db
+#		Mysql
+#		Php 
+#		Phpmyadmin
+#		Sublime
+#		Mozilla
+#		Chrome
+#**/
+
 #!/bin/bash
 
 
+#color
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+
 if [[ $EUID -ne 0 ]]; then
-   echo "Please Change User To Root" 
+   echo "Please Change User To ${RED}Root" 
    exit 1
 fi
 
@@ -10,61 +42,62 @@ fi
 apache ()
 {
 	clear
-	echo "Installing Apache2"
+	echo "Installing ${BLUE}Apache2"
 	apt-get install apache2
-	echo "Apache2 Installation Complete"
+	echo "${BLUE}Apache2 Installation ${GREEN}Complete"
 	
 }
 nginx ()
 {
 	clear
-	echo "Installing Nginx..."
+	echo "Installing ${BLUE}Nginx..."
 	apt-get install nginx
-	echo "Nginx.. Installation Complete!!"
+	echo "${BLUE}Nginx.. Installation ${GREEN}Complete!!"
 	echo "You Can Now Configure Nginx"
 }
 
 mariadb ()
 {
 	clear
-	echo "Installing Maria-Db"	
+	echo "Installing ${BLUE}Maria-Db"	
 	apt-get install mariadb-server mariadb-client
-	echo "Maria-Db Installation Complete"
+	echo "${BLUE}Maria-Db Installation ${GREEN}Complete"
 }
 
 php ()
 {
 	clear
-	echo "Installing PHP Latest Version Avaialble"
+	echo "Installing ${BLUE}PHP Latest Version Avaialble"
 	apt-get install php-fpm php-mysql
-	echo "Installing Essential PHP Mods"
+	echo "Installing ${BLUE}Essential PHP Mods"
 	apt-get install php7.0-curl
-	echo "PHP Installation Complete"
+	echo "${BLUE}PHP Installation ${GREEN}Complete"
 }
 
 mysql ()
 {
 	clear
-	echo "Installing Mysql Server"
+	echo "Installing ${BLUE}Mysql Server"
 	apt-get install mysql-server mysql-client
-	echo "Mysql Installation Complete"
+	echo "${BLUE}Mysql Installation ${GREEN}Complete"
 }
 
 phpmyadmin ()
 {
 	clear
-	echo "Installing phpmyadmin"
+	echo "Installing ${BLUE}phpmyadmin"
 	apt-get install phpmyadmin
 	ln -s /usr/share/phpmyadmin/ /var/www/html
+	echo "${BLUE}Phpmyadmin Installation ${GREEN}Complete"
 }
 
 
 git ()
 {
 	clear
-	echo "Installing GIT"
+	echo "Installing ${BLUE}GIT"
 	apt-get install git
-	echo "GIT Installed..."
+	echo "${BLUE}GIT Installation ${GREEN}Complete..."
 }
 
 sublime ()
@@ -72,10 +105,10 @@ sublime ()
 	clear
 	echo "Getting Sublime For You.."
 	wget -O /tmp/sublime-text_build-3126_amd64.deb https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
-	echo "Installing Sublime...."
+	echo "Installing ${BLUE}Sublime...."
 	dpkg -i /tmp/sublime-text_build-3126_amd64.deb
 	apt-get install -f
-	echo "Sublime Installed"
+	echo "${BLUE}Sublime Installation ${GREEN}Complete"
 }
 
 
@@ -85,10 +118,10 @@ chrome ()
 	clear
 	echo "Getting Chrome For You"
 	wget -O /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	echo "Installing...."
+	echo "Installing.${BLUE}..."
 	dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 	apt-get install -f
-	echo "Chrome Installed"
+	echo "${BLUE}Chrome Installation ${GREEN}Complete"
 }
 
 mozilla ()
@@ -96,11 +129,11 @@ mozilla ()
 	clear	
 	echo "Getting Firefox For You"
 	wget -O /tmp/firefox-54.0b7.tar.bz2 http://ftp.mozilla.org/pub/firefox/releases/54.0b7/linux-x86_64/en-US/firefox-54.0b7.tar.bz2
-	echo "Installing...."
+	echo "Installing.${BLUE}..."
 	mv /tmp/firefox-54.0b7.tar.bz2 /opt
 	tar xvjf firefox-54.0b7.tar.bz2
 	ln -s /opt/firefox/firefox /usr/bin/firefox
-	echo "Firefox Installed"
+	echo "${BLUE}Firefox Installation ${GREEN}Complete"
 
 }
 
@@ -108,13 +141,15 @@ mozilla ()
 
 restart ()
 {
+clear
+echo "Your Services Will Restart Now And Your ${RED}/tmp will be ${GREEn}cleared"
 service apache2 restart
 service nginx restart
 service php-fpm restart
 service mysql restart
 apt autoremove
 apt-get clean
-reboot
+rm -rf /tmp/*
 }
 
 
@@ -175,8 +210,6 @@ Lemp ()
 	php
 	phpmyadmin
 	git
-	browser
-	ide
 
 }
 
@@ -187,8 +220,7 @@ Lamp ()
 	php
 	phpmyadmin
 	git
-	browser
-	ide
+
 }
 
 echo "Enter"
@@ -203,7 +235,7 @@ while true; do
         [1]* ) Lemp; break;;
         [2]* ) Lamp; break;;
 		[exitEXITExit]* ) exit;;
-        * ) echo "Please answer from options Available \n";;
+        * ) echo "Please answer from options ${BLUE}Available \n";;
 	esac
 done
 
@@ -219,11 +251,21 @@ while true; do
 done
 
 
+while true; do
+    read -p "Do You Need A IDE...?" yn
+    case $yn in
+        [Yy]* ) ide; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
 
 
 
 while true; do
-    read -p "Have You Configured Your Development Enviornment And Apply Settings..?" yn
+    read -p "Have You Configured Your Development Enviornment And Ready To  Apply Settings..?" yn
     case $yn in
         [Yy]* ) restart; break;;
         [Nn]* ) exit;;
